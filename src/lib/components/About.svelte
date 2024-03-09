caa30ad7498bebfb51a6498c29afc637f0b44013
Manav510 <98536459+Manav510@users.noreply.github.com>
2024-03-09 22:51:32 +0530

    favicon addition

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -4,15 +4,15 @@
     let userData = getContext('userDataContext');
   </script>
   
-  <div class="flex flex-col p-5 w-full max-w-[864px] h-[238px] my-4 relative">
-    <img src={userData.user.profilePicture} alt={`Profile picture of ${userData.user.name}`} class="rounded-full border-4 border-[#EAEFF5] absolute w-[120px] h-[120px] top-[23px] left-4" />
-    <h2 class="font-inter font-bold text-2xl text-center absolute w-[156px] h-[56px] top-[173px] left-[11px]">{userData.user.name}</h2>
-    <div class="flex flex-col p-5 w-[864px] h-[238px] relative my-4 gap-5" style="top: 22px; left: 211px;">
+  <div class="flex flex-col p-5 w-full max-w-[864px] h-[238px] my-4 relative ">
+    <img src={userData.user.profilePicture} alt={`Profile picture of ${userData.user.name}`} class="rounded-full border-4 border-[#EAEFF5] absolute w-[120px] h-[120px] top-[23px] left-[31px]" />
+    <h2 class="font-inter font-bold text-2xl text-center absolute w-[156px] h-[56px] top-[153px] left-[11px]">{userData.user.name}</h2>
+    <div class="flex flex-col p-5 w-[864px] h-[138px] relative my-4 gap-5 top-[-2px] left-[211px]" style="">
     <div class="flex justify-between items-start relative flex-row w-full h-[82px]">
       <p class="w-[592px] h-[72px] pr-2">{userData.user.bio}</p>
       
       <div class="flex flex-col items-end w-[140px] h-[72px] relative">
-        <img src={userData.user.logo} alt="duggup-logo" class="rounded-full mb-2 w-[30px] h-[30px]" />
+        <img src={userData.user.logo} alt="duggup-logo" class="rounded-full mb-2 w-[40px] h-[40px]" />
         <h5>{userData.user.company}</h5>
         <p class="text-sm text-gray-600 mt-2">{userData.user.title}</p>
       </div>
